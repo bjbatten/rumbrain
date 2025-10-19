e70978ca3ad47d04f5a31ed23647735909e61935
Brian Batten <brian.batten@jll.com>
2025-10-18 21:43:08 -0400

    chore: add /healthz endpoint for readiness probes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/healthz", to: proc { [ 200, { "Content-Type"=>"application/json" }, [ { ok: true }.to_json ] ] }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
